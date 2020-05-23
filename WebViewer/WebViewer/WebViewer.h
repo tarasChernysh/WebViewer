29174a0b821c211954016f02909938bda2954ca7
TarasChernysh <chernyshtaras94@gmail.com>
2020-05-23 21:23:53 +0300

    Initial commit

---
+++ WebViewer/WebViewer/WebViewer.h
@@ -0,0 +1,19 @@
+//
+//  WebViewer.h
+//  WebViewer
+//
+//  Created by Taras Chernysh on 23.05.2020.
+//  Copyright © 2020 Taras Chernysh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebViewer.
+FOUNDATION_EXPORT double WebViewerVersionNumber;
+
+//! Project version string for WebViewer.
+FOUNDATION_EXPORT const unsigned char WebViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebViewer/PublicHeader.h>
+
+
